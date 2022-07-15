4da91821b361ed0d1a3dafd9c46c9a2033f10655
rong3000 <rong.lin3000@gmail.com>
2022-07-15 10:59:16 +1000

    checked against cfweb3 and plbackup

--- contract/contracts/pl.sol
+++ contract/contracts/pl.sol
@@ -12,6 +12,25 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/security/PullPayment.sol";
 
+abstract contract ContextMixin {
+    function msgSender() internal view returns (address payable sender) {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = payable(msg.sender);
+        }
+        return sender;
+    }
+}
+
 contract PL1155 is
     ERC1155,
     Ownable,
@@ -20,16 +39,18 @@ contract PL1155 is
     ERC1155Supply,
     ERC1155URIStorage,
     EIP712,
-    PullPayment
+    PullPayment,
+    ContextMixin
 {
     using ECDSA for bytes32;
 
     string storeMetaURL = "https://metapython.herokuapp.com/api/store/";
     string private constant SIGNING_DOMAIN = "PL"; //VIDEO
     string private constant SIGNATURE_VERSION = "1"; //VIDEO
-    uint256 public cost_per_token_set = 0.1 ether; //
+    uint256 cost_per_token_set = 0.05 ether; //
+    string baseURI = "https://metapython.herokuapp.com/api/element/{id}";
 
-    function contractURI() public view returns (string memory) {
+    function getContractURI() public view returns (string memory) {
         return storeMetaURL;
     }
 
@@ -41,9 +62,15 @@ contract PL1155 is
         cost_per_token_set = _cost_per_token_set;
     }
 
-    using SafeMath for uint256;
+    function getCost() public view returns (uint256){
+        return cost_per_token_set;
+    }
 
-    uint256 public MAX_MINTS = 204800;
+    using SafeMath for uint256;
+    uint256 MAX_MINTS = 20480;
+    uint256 tokensetMinted = 0;
+    bool hasSaleStarted = false;
+
     address proxyAddress = 0x1E525EEAF261cA41b809884CBDE9DD9E1619573A;//rinkeby
     // address proxyAddress = 0xa5409ec958c83c3f309868babaca7c86dcb077c1;//mainnet
 
@@ -62,7 +89,7 @@ contract PL1155 is
     address private _ranSignerAddress;
 
     constructor(address signerAddress_, address ranSignerAddress_)
-        ERC1155("https://metapython.herokuapp.com/api/element/{id}")
+        ERC1155(baseURI)
         EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION)
     {
         _signerAddress = signerAddress_;
@@ -81,13 +108,21 @@ contract PL1155 is
         proxyAddress = _proxyAddress;
     }
 
-    function getProxy() public view onlyOwner returns (address) {
+    function getProxy() public view returns (address) {
         return proxyAddress;
     }
 
     function setMaxMints(uint256 max_mints) public onlyOwner {
         MAX_MINTS = max_mints;
     }
+
+    function getMaxMints() public view returns (uint256) {
+        return MAX_MINTS;
+    }   
+
+    function getHasSaleStarted() public view returns (bool) {
+        return hasSaleStarted;
+    }    
 
     function pause() public onlyOwner {
         _pause();
@@ -97,11 +132,32 @@ contract PL1155 is
         _unpause();
     }
 
+    function startSale() public onlyOwner {
+        hasSaleStarted = true;
+    }
+
+    function pauseSale() public onlyOwner {
+        hasSaleStarted = false;
+    }
+
+    function getTokensetMinted() public view returns (uint256) {
+        return tokensetMinted;
+    }    
+
+    function hasSoldOut() public view returns (bool) {
+        if (getTokensetMinted() >= MAX_MINTS) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     function mint(
         string memory ranId,
         uint256 randomNumber,
         bytes memory ranSignature
     ) public payable {
+        require(hasSaleStarted || msg.sender == owner(), "sale hasn't started");
         require(msg.value == cost_per_token_set * 3, "Incorrect Ether amount.");
         require(ranConsumed[ranId] + 1 <= 1, "Random number already used.");
         require(randomNumber > 0, "Id cannot be 0");
@@ -111,10 +167,15 @@ contract PL1155 is
                 _ranSignerAddress,
             "Random number invalid"
         );
+        require(
+            tokensetMinted + 1 <= MAX_MINTS || msg.sender == owner(),
+            "sold out"
+        );
 
         _mint(msg.sender, randomNumber, 1, "0x00");
         emit Minted(msg.sender, randomNumber, 1);
         ranConsumed[ranId] += 1;
+        tokensetMinted +=1;
     }
 
     function wlMint(
@@ -125,6 +186,7 @@ contract PL1155 is
         uint256 randomNumber,
         bytes memory ranSignature
     ) public payable {
+        require(hasSaleStarted || msg.sender == owner(), "sale hasn't started");
         require(msg.value == cost_per_token_set, "Incorrect Ether amount.");
         require(maxWLTokenNum >= wlConsumed[wlId] + 1, "WL limit exceeded.");
         require(ranConsumed[ranId] + 1 <= 1, "Random number already used.");
@@ -133,18 +195,23 @@ contract PL1155 is
         require(
             verify(wlId, maxWLTokenNum, msg.sender, wlSignature) ==
                 _signerAddress,
-            "Voucher invalid"
+            "Whitelist voucher invalid"
         );
         require(
             verify(ranId, randomNumber, msg.sender, ranSignature) ==
                 _ranSignerAddress,
             "Random number invalid"
         );
+        require(
+            tokensetMinted + 1 <= MAX_MINTS || msg.sender == owner(),
+            "sold out"
+        );
 
         _mint(msg.sender, randomNumber, 1, "0x00");
         emit wlMinted(msg.sender, randomNumber, 1);
         wlConsumed[wlId] += 1;
         ranConsumed[ranId] += 1;
+        tokensetMinted +=1;
     }
 
     function permanentURI(string memory _value, uint256 _id) public onlyOwner {
@@ -152,7 +219,6 @@ contract PL1155 is
     }
 
     function merge(
-        address account,
         uint256[] memory ids,
         bytes memory data
     ) public {
@@ -171,13 +237,12 @@ contract PL1155 is
         }
 
         require(id > 10000, "Merged id cannot be basic element id");
-        _burnBatch(account, ids, amounts);
-        _mint(account, id, 1, data);
-        emit Merged(account, id, 1);
+        _burnBatch(msg.sender, ids, amounts);
+        _mint(msg.sender, id, 1, data);
+        emit Merged(msg.sender, id, 1);
     }
 
     function split(
-        address to,
         uint256 id,
         bytes memory data
     ) public {
@@ -191,7 +256,7 @@ contract PL1155 is
         }
         require(digits < 11, "Cannot be split into more than 10 elements");
 
-        _burn(to, id, 1);
+        _burn(msg.sender, id, 1);
 
         uint256[] memory ids = new uint256[](digits);
         uint256[] memory amounts = new uint256[](digits);
@@ -202,8 +267,8 @@ contract PL1155 is
             id = id / 10000;
         }
 
-        _mintBatch(to, ids, amounts, data);
-        emit Split(to, originalId);
+        _mintBatch(msg.sender, ids, amounts, data);
+        emit Split(msg.sender, originalId);
     }
 
     function mintBatch(
@@ -212,10 +277,7 @@ contract PL1155 is
         uint256[] memory amounts,
         bytes memory data
     ) public onlyOwner {
-        for (uint256 i = 0; i < ids.length; i++) {
-            require(ids[i] > 0, "Id cannot be 0");
-            require(ids[i] < 10000, "Id must be smaller than 10000");
-        }
+        require(hasSaleStarted || msg.sender == owner(), "sale hasn't started");
         _mintBatch(to, ids, amounts, data);
         emit MintedBatch(to, ids, amounts);
     }
@@ -259,6 +321,10 @@ contract PL1155 is
         _setURI(newuri);
     }
 
+    function getURI() public view returns (string memory)  {
+        return baseURI;
+    }
+
     function setBaseURI(string memory baseURI) public onlyOwner {
         _setBaseURI(baseURI);
     }
@@ -286,13 +352,17 @@ contract PL1155 is
         _asyncTransfer(dest, amount);
     }
 
+    function _msgSender() internal view override returns (address sender) {
+        return ContextMixin.msgSender();
+    }
+
     function verify(
         // uint256 id,
         string memory id,
         uint256 number,
         address minterAddress,
         bytes memory signature
-    ) public view returns (address) {
+    ) public onlyOwner view returns (address) {
         bytes32 digest = _hashTypedDataV4(
             keccak256(
                 abi.encode(
